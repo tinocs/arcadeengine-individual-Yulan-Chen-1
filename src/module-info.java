b1ea7b85384f2e7da06d7cdf9030df582f3bdc94
Yulan Chen <ychen332@student.fuhsd.org>
2026-04-07 09:38:33 -0700

    Setup part 1

---
+++ src/module-info.java
@@ -0,0 +1,14 @@
+open module P1_Chen_Elaine_ArcadeEngine{
+	// Require each of the javafx modules you need to use.
+    // Include at least javafx.graphics, javafx.controls and javafx.fxml.
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    // You may want to require javafx.media if you use sound or videos
+    requires javafx.media;
+
+    // You may want to require javafx.web if you use a WebView
+    requires javafx.web;
+	requires javafx.base;
+}
